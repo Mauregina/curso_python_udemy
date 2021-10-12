1768fbf9cc2c145e993d74d8e929cb2e2ac95847
Maura Regina <mauraregina@gmail.com>
2021-10-12 18:28:09 -0400

    Adiciona arquivo com funcoes uteis
    Signed-off-by: Maura Regina <mauraregina@gmail.com>

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+import sys, time, os
+
+def deleta_ultimas_linhas(qtde_linhas, espera: int):
+    sobe_cursor = '\x1b[1A'
+    apaga_linha = '\x1b[2K'
+
+    time.sleep(espera)
+
+    for _ in range(qtde_linhas):
+        sys.stdout.write(sobe_cursor)
+        sys.stdout.write(apaga_linha)
+
+def limpar_tela():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
