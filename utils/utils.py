5ce5227fd6a8dfbfc715259cf46631e6fabcdfa3
Maura Regina <mauraregina@gmail.com>
2021-10-12 22:23:46 -0400

    Alterar nome da funcao para limpar tela
    Signed-off-by: Maura Regina <mauraregina@gmail.com>

--- utils/utils.py
+++ utils/utils.py
@@ -10,7 +10,7 @@ def deleta_ultimas_linhas(qtde_linhas, espera: int):
         sys.stdout.write(sobe_cursor)
         sys.stdout.write(apaga_linha)
 
-def limpar_tela():
+def limpa_tela():
     if os.name == 'nt':
         os.system('cls')
     else:
